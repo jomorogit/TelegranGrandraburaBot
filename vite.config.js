9eb73e95bce3c469dbcc430f920d6f13f03c3c88
jomorogit <jomoroforwork@gmail.com>
2026-01-05 01:11:55 +0100

    prepare for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TelegranGrandraburaBot/',
+  // base: '/...' <-- ЭТУ СТРОКУ МЫ УБРАЛИ, ОНА БОЛЬШЕ НЕ НУЖНА
 })
