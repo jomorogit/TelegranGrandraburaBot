9299761c78a993001da95145b8dfeb6e1a80bd02
jomorogit <jomoroforwork@gmail.com>
2026-01-05 02:21:56 +0100

    описание того, что ты изменил

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// 1. Импортируем Router
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2. Оборачиваем App и указываем имя ТВОЕГО репозитория */}
+    {/* Важно: имя должно быть точь-в-точь как на GitHub, без слеша в конце */}
+    <BrowserRouter basename="/TelegranGrandraburaBot">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
